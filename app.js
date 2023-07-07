99c6d8046dded3951915fc65d07122d053b8e6ac
Ishan Bajaj <ishanb007@gmail.com>
2023-07-08 02:35:51 +0530

    Functionality of adding list items in to do listlist items in to do list

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var item = [];
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+
+    var today= new Date();
+    
+    var options ={
+        weekday: "long",
+        day:"numeric",
+        month:"long"
+    };
+   var day = today.toLocaleDateString("en-US",options);
+    var dayHin = today.toLocaleDateString("hi-IN",options);
+    
+    res.render("list",{dayHin:dayHin,day:day,newListItem: item});
+});
+
+app.post("/",function(req,res){
+    item.push(req.body.newItem);
+    res.redirect("/");
+});
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+})
